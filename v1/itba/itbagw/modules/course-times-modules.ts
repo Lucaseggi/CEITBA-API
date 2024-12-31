b6fd488a9a298e474742f9a46cadd096245ff0f8
Ian <daltioan@gmail.com>
2024-12-31 19:20:28 -0300

    Add course commission data fetching and upsert functionality

---
+++ v1/itba/itbagw/modules/course-times-modules.ts
@@ -0,0 +1,40 @@
+export interface Root {
+    courseCommissions: CourseCommissions
+  }
+  
+  export interface CourseCommissions {
+    courseCommission: CourseCommission[]
+  }
+  
+  export interface CourseCommission {
+    subjectCode: string
+    subjectName: string
+    subjectType: string
+    courseStart: string
+    courseEnd: string
+    commissionName: string
+    commissionId: string
+    quota: string
+    enrolledStudents: string
+    courseCommissionTimes: any
+  }
+  
+  export interface Comissions{
+    subject_code: string
+    subject_type: string
+    course_start: string
+    course_end: string
+    comission_name: string
+    id: string
+    quota: string
+    enrolled_students: string
+  }
+
+  export interface CourseCommissionTime {
+    course_id:string
+    day: string
+    class_room: string
+    building: string
+    hour_from: string
+    hour_to: string
+  }
--- v1/itba/itbagw/update.ts
+++ v1/itba/itbagw/update.ts
@@ -4,6 +4,7 @@ import { Database,Tables } from "../ceitbapi/database.types";
 import { createClient } from "@supabase/supabase-js";
 import { ITBA_API_TOKEN, SUPABASE_ACCESS_TOKEN } from "../../../server";
 import { DatabaseSubjectPlan, Subject, SubjectPlan } from "../ceitbapi/modules";
+import { Root,CourseCommission ,Comissions,CourseCommissionTime} from "./modules/course-times-modules";
 
 async function UpdateSubjects() {
     const supabase = createClient("https://yafawebqzogkzwhxojbh.supabase.co", SUPABASE_ACCESS_TOKEN);
@@ -64,10 +65,58 @@ async function UpdateCommissions(){
     const url = `https://itbagw.itba.edu.ar/api/v1/courseCommissions/${ITBA_API_TOKEN}?level=GRADUATE&year=2025&period=FirstSemester`;
 
     const data = await fetch(url);
+    const jsonData: Root = await data.json();
+
+    const comissions:Comissions[]=[];
+    const comissionTimes:CourseCommissionTime[]=[];
+
+    for (const course of jsonData.courseCommissions.courseCommission) {
+        course.courseCommissionTimes = course.courseCommissionTimes instanceof Array? course.courseCommissionTimes: [course.courseCommissionTimes];
+        comissions.push({
+            subject_code:course.subjectCode,
+            subject_type:course.subjectType,
+            course_start:course.courseStart,
+            course_end:course.courseEnd,
+            comission_name:course.commissionName,
+            id:course.commissionId,
+            quota:course.quota,
+            enrolled_students:course.enrolledStudents})
+
+        
+
+        for (const time of course.courseCommissionTimes){
+            if (time == undefined) continue;
+            comissionTimes.push({
+                course_id:course.commissionId,
+                day:time.day,
+                class_room:time.classRoom,
+                building:time.building,
+                hour_from:time.hourFrom,
+                hour_to:time.hourTo
+            });
+        }
+
+    }
+
+    await supabase
+        .from('course')
+        .upsert(comissions, { ignoreDuplicates: true })
+        .select().then((data) => { console.log(data) });
+
+    await supabase
+    .from('course_time')
+    .upsert(comissionTimes, { ignoreDuplicates: true })
+    .select().then((data) => {
+        if (data.error != null) {
+            console.log(data);
+        }
+    });
     
     
 }
 
 
-UpdateSubjects();
+//UpdateSubjects();
+
+UpdateCommissions();
 
